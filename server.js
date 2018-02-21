24660907259c2fcd766b8ba48809294b9c6ff3cd
Sangwonlee93 <lsw0833@hanmail.net>
2018-02-21 20:04:51 +0900

    broker backup

---
+++ server.js
@@ -0,0 +1,46 @@
+var mosca = require('mosca');
+var mqtt = require('mqtt');
+var connectManager;
+var brokerId = "uniq1";
+var settings = {
+  port: 3001
+};
+var server = new mosca.Server(settings);
+server.on('ready', setup); //on init it fires up setup()
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Borker is up and running');
+  connectManager = mqtt.connect('tcp://127.0.0.1:8000');
+  connectManager.on('connect', () => {
+    connectManager.subscribe('#');
+  });
+  connectManager.on('message', (topic, message) => {
+    var slashindex = topic.indexOf("/") + 1;
+    if (brokerId != topic.slice(0,slashindex-1)) {
+      var realmsg = {
+        topic: topic.slice(slashindex, topic.length),
+        payload: message, // or a Buffer
+        qos: 1, // 0, 1, or 2
+        retain: false, // or true
+      };
+      server.publish(realmsg,"ComeToBrokerManager", function() {
+        console.log('done!');
+      });
+    }
+  });
+}
+server.on('published', function(packet, client) {
+  if (packet.topic.indexOf("$SYS") != 0 && packet.topic != "internal") {
+    connectManager.publish(brokerId + "/" + packet.topic, packet.payload);
+  }
+});
+// fired when a client connects
+server.on('clientConnected', function(client) {
+  console.log('Client Connected:', client.id);
+});
+
+// fired when a client disconnects
+server.on('clientDisconnected', function(client) {
+  console.log('Client Disconnected:', client.id);
+});
